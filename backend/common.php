43075558c177191f78bfa7b125177c9f492c33b5
Rashe <shemeshra@gmail.com>
2017-02-15 20:52:57 +0200

    Update common.php

--- backend/common.php
+++ backend/common.php
@@ -1,10 +1,7 @@
 <?php
 
 // These variables define the connection information for your MySQL database
-$username = "root";
-$password = "Omenius2";
-$host = "localhost";
-$dbname = "adTemplate";
+
 
 // UTF-8 is a character encoding scheme that allows you to conveniently store
 // a wide varienty of special characters, like ¢ or €, in your database.
